bec5114c6de45660e1c94f85f49664628c60b52f
Ronald d'Hamecourt <Ronald.dhamecourt@caseware.com>
2024-03-29 15:35:49 +0100

    upgrade jersey

--- nl.c2c.ac.api2/src/main/java/nl/c2c/ac/api2/application/servlet/ac/HttpAcServlet.java
+++ nl.c2c.ac.api2/src/main/java/nl/c2c/ac/api2/application/servlet/ac/HttpAcServlet.java
@@ -2,31 +2,34 @@ package nl.c2c.ac.api2.application.servlet.ac;
 
 import lombok.extern.slf4j.Slf4j;
 import lotus.domino.NotesThread;
-import org.glassfish.jersey.servlet.ServletContainer;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
-/**
- * The equivalent of the OSGi Http Services "servlet" in registerServlet. The Servlet is instantiated by the Extension Point provider and must be an instance of javax.servlet.Servlet
- */
+//Called by com.ibm.ws.webcontainer.servlet
 @Slf4j
-public class HttpAcServlet extends ServletContainer {
+public class HttpAcServlet extends HttpServlet { //TODO this was old version of Jersey ServletContainer {
 
     private static final long serialVersionUID = 1L;
 
     public HttpAcServlet() {
-        super(new ResourcesAcConfiguration());
+        //TODO Jersey was responsible for our resources
+//        super(new ResourcesAcConfiguration());
     }
 
-    @Override
     public void service(final ServletRequest req, final ServletResponse res) throws ServletException, IOException {
         try {
             NotesThread.sinitThread();
-            super.service(req, res);
+            //TODO here we did redirect o jersey code
+            // super.service(req, res);
+            try (PrintWriter writer = res.getWriter()) {
+                writer.println("Hello, world!");
+            }
         } catch (final Exception exception) {
             log.error(exception.getMessage(), exception);
         } finally {
--- nl.c2c.ac.api2/src/main/java/nl/c2c/ac/api2/resources/actions/TestController.java
+++ nl.c2c.ac.api2/src/main/java/nl/c2c/ac/api2/resources/actions/TestController.java
@@ -1,11 +1,12 @@
 package nl.c2c.ac.api2.resources.actions;
 
+//TODO jakararta!!
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 import nl.c2c.ac.service.Testservice;
 
